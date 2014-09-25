4d21548176f53330b7269954fb3ce9525350db77
wrobstory <wrobstory@gmail.com>
2014-09-25 16:28:32 -0700

    ENH: Add DataFrame conveniences

--- malort/core.py
+++ malort/core.py
@@ -22,15 +22,15 @@ from malort.type_mappers import TypeMappers
 def analyze(path, delimiter='\n'):
 
     stats = {}
-    for blob in dict_generator(path, delimiter):
+    for count, blob in enumerate(dict_generator(path, delimiter)):
         recur_dict(blob, stats)
 
-    return MalortStats(stats)
+    return MalortStats(stats, count)
 
 
 class MalortStats(TypeMappers):
 
-    def __init__(self, stats):
+    def __init__(self, stats, blob_count):
         """
         Wrapper for malort stats that can generate type maps and
         DataFrames
@@ -39,8 +39,11 @@ class MalortStats(TypeMappers):
         ----------
         stats: dict
             Malort stats dict
+        blob_count: int
+            Number of JSON blobs read into result
         """
         self.stats = stats
+        self.count = blob_count
 
     def get_conflicting_types(self):
         """Return only the stats where there are multiple types detected"""
@@ -50,3 +53,35 @@ class MalortStats(TypeMappers):
                 conflicted[k] = v
 
         return conflicted
+
+    def to_dataframe(self, include_db_types=True):
+        """
+        Export stats dict to a Pandas DataFrame
+
+        Parameters
+        ----------
+        include_db_types: boolean, default True
+            Include database type inference in DataFrame
+        """
+        import pandas as pd
+
+        df_cols = ['key', 'count', 'type', 'mean', 'max', 'min', 'sample']
+
+        if include_db_types:
+            db_type_getters = [('redshift_types', self.get_redshift_types)]
+            type_results = [(t[0], t[1]()) for t in db_type_getters]
+            df_cols.extend([t[0] for t in db_type_getters])
+
+        # Get the data into a to_dict-able format
+        dictable = {}
+        for i, (key, value) in enumerate(self.stats.items()):
+            for ktype, stats in value.items():
+                stats.update({'key': key, 'type': ktype})
+                if include_db_types:
+                    for name, result in type_results:
+                        stats.update({name: result[key]})
+                dictable[i] = stats
+
+        df = pd.DataFrame.from_dict(dictable, 'index')
+
+        return df.reindex_axis(df_cols, axis=1)
