ee76bd09e56723280ebdd7c03a1a0ee4a75c947b
wrobstory <wrobstory@gmail.com>
2014-09-24 17:06:59 -0700

    ENH: Add stats updater method, tests

--- malort/core.py
+++ malort/core.py
@@ -15,7 +15,7 @@ import os
 from os.path import isfile, join, splitext
 
 
-__all__ = ('dict_generator')
+__all__ = ('dict_generator', 'update_entry_stats')
 
 
 def dict_generator(path, delimiter='\n'):
@@ -49,3 +49,52 @@ def dict_generator(path, delimiter='\n'):
                 else:
                     yield json.loads(fread.read())
 
+value = {
+    "str": {'count': 5, 'avg_len': 6},
+    "int": {'max': 67, 'min': 0, 'mean': 5, 'count': 8},
+    "float": {'max': 6.7, 'min': -1.3, 'count': 6},
+    "boolean": {'count': 6}
+}
+
+
+def get_new_mean(value, current_mean, count):
+    """Given a value, current mean, and count, return new mean"""
+    summed = current_mean * count
+    return (summed + value)/(count + 1)
+
+
+def update_entry_stats(value, current_stats):
+    """
+    Given a value and a dict of current statistics, return a dict of new
+    statistics for the given value type. Values for str reference their len.
+
+    Ex:
+    current_stats = {
+        "str": {'count': 2, 'max': 2, 'min': 1, 'mean': 1.5},
+        "int": {'count': 1, 'max': 5, 'min': 5, 'mean': 5},
+    }
+    update_entry_stats(10, current_stats)
+    {'count': 2, 'max': 10, 'min': 5, 'mean': 7.5}
+
+    Parameters
+    ----------
+    value: str, int, float, boolean
+    current_stats: Dict, see Example
+    """
+    value_type = type(value).__name__
+    new_stats = {}
+    stats = current_stats.get(value_type, {})
+    if value_type == 'str':
+        value = len(value)
+    count = stats.get('count', 0)
+    if value_type in ['str', 'int', 'float']:
+        max_, min_, mean = (stats.get('max', value),
+                            stats.get('min', value),
+                            stats.get('mean', 0))
+
+        new_stats['mean'] = round(get_new_mean(value, mean, count), 3)
+        new_stats['max'] = value if max_ < value else max_
+        new_stats['min'] = value if min_ > value else min_
+    new_stats['count'] = count + 1
+
+    return new_stats
--- malort/tests/test_malort.py
+++ malort/tests/test_malort.py
@@ -26,3 +26,52 @@ class TestDictGenerator(unittest.TestCase):
     def test_json_files_pipe(self):
         gen = mt.core.dict_generator(TEST_FILES_2, '|')
         self.assertEquals(len([d for d in gen]), 4)
+
+
+class TestUpdateEntryStats(unittest.TestCase):
+
+    def test_stats_str(self):
+        update_1 = mt.core.update_entry_stats('Foooo', {})
+        self.assertEquals(update_1,
+                         {'count': 1, 'mean': 5.0, 'max': 5,
+                          'min': 5})
+
+        update_2 = mt.core.update_entry_stats('Foooo', {'str': update_1})
+        self.assertEquals(update_2,
+                          {'count': 2, 'mean': 5.0, 'max': 5,
+                           'min': 5})
+
+        update_3 = mt.core.update_entry_stats('Foo', {'str': update_2})
+        self.assertEquals(update_3,
+                          {'count': 3, 'mean': 4.333, 'max': 5,
+                           'min': 3})
+
+    def test_stats_bool(self):
+        update_1 = mt.core.update_entry_stats(True, {})
+        self.assertEquals(update_1, {'count': 1})
+
+        update_2 = mt.core.update_entry_stats(False, {'bool': update_1})
+        self.assertEquals(update_2, {'count': 2})
+
+    def test_stats_number(self):
+        update_1 = mt.core.update_entry_stats(1, {})
+        self.assertEquals(update_1,
+                          {'count': 1, 'mean': 1.0, 'max': 1,
+                           'min': 1})
+
+        update_2 = mt.core.update_entry_stats(2.0, {'int': update_1})
+        self.assertEquals(update_2,
+                          {'count': 1, 'mean': 2.0, 'max': 2.0,
+                           'min': 2.0})
+
+        update_3 = mt.core.update_entry_stats(2, {'int': update_1,
+                                                  'float': update_2})
+        self.assertEquals(update_3,
+                          {'count': 2, 'mean': 1.5, 'max': 2,
+                           'min': 1})
+
+        update_4 = mt.core.update_entry_stats(4.0, {'int': update_3,
+                                                    'float': update_2})
+        self.assertEquals(update_4,
+                          {'count': 2, 'mean': 3.0, 'max': 4.0,
+                           'min': 2.0})
