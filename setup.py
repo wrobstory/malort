f739137d1784886c0bd051d06eabc760859bf5b1
wrobstory <wrobstory@gmail.com>
2015-08-14 14:11:05 -0700

    DIST: Add setup reqs

--- setup.py
+++ setup.py
@@ -5,6 +5,17 @@ try:
 except ImportError:
     from distutils.core import setup
 
+reqs = ["dask>=0.7.0",
+        "dill>=0.2.4",
+        "numpy>=1.9.2",
+        "pandas>=0.16.2",
+        "python-dateutil>=2.4.2",
+        "pytz>=2015.4",
+        "six>=1.9.0",
+        "toolz>=0.7.2",
+        "wheel>=0.24.0"]
+
+
 setup(
     name='malort',
     version='0.0.4',
@@ -19,5 +30,6 @@ setup(
                  'Programming Language :: Python :: 2',
                  'Programming Language :: Python :: 3',
                  'License :: OSI Approved :: MIT License'],
-    packages=['malort']
+    packages=['malort'],
+    install_requires=reqs
 )
